d14dfb4856b775a04aea9839fcc520d3d71c57df
Thunder <thunderleng@gmail.com>
2023-06-26 14:47:57 +0800

    addscss

--- src/components/login.vue
+++ src/components/login.vue
@@ -16,11 +16,28 @@
 </script>
 
 <template>
-  <el-button type="primary">Primary</el-button>
-  <authenticator>
-    <template v-slot="{ user, signOut }">
-      <h1>Hello {{ user.username }}!</h1>
-      <button @click="signOut">Sign Out</button>
-    </template>
-  </authenticator>
+  <div class="content"> 
+    <el-button type="primary">Primary</el-button>
+    <authenticator>
+      <template v-slot="{ user, signOut }">
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+      </template>
+    </authenticator>
+  </div>
+
 </template>
+
+
+<style lang="scss" scoped>
+.content{
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(rgba(255, 204, 103, 0.196) 0%, rgba(0, 145, 132, 0.3) 100%);
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+}
+</style>
